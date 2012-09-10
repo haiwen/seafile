8d91ac724b0361c1fb7635a1ae7d048e157a4e34
lins05 <mysnowls@163.com>
2012-09-10 14:34:57 +0800

    enhance server release scripts

--- tools/pack-server.sh
+++ tools/pack-server.sh
@@ -21,15 +21,19 @@ if [[ ! ${version} =~ [0-9]\.[0-9]\.[0-9] ]]; then
     exit 1;
 fi
 
-server_dir=seafile-server-${version}
+# server_dir=seafile-server-${version}
 
-if [[ ! -d ${server_dir} ]]; then
-    echo
-    echo "The directory \"${server_dir}\" does not exist"
-    echo
-    exit 1;
-fi
+# if [[ ! -d ${server_dir} ]]; then
+#     echo
+#     echo "The directory \"${server_dir}\" does not exist"
+#     echo
+#     exit 1;
+# fi
 
 serverpath=seafile-server-${version}
 
-tar czhvf seafile-server_${version}_x86-64.tar.gz ${serverpath} --exclude-vcs --exclude=${serverpath}/seafile/share* --exclude=${serverpath}/seafile/include*  --exclude=${serverpath}/runtime/*.log --exclude=${serverpath}/runtime/*.pid --exclude=${serverpath}/seahub/seahub.db --exclude=${serverpath}/seahub/avatar/testdata
+mv seafile-server ${serverpath}
+
+tar czvf seafile-server_${version}_x86-64.tar.gz ${serverpath} --exclude-vcs --exclude=${serverpath}/seafile/share* --exclude=${serverpath}/seafile/include*  --exclude=${serverpath}/runtime/*.log --exclude=${serverpath}/runtime/*.pid --exclude=${serverpath}/seahub/seahub.db --exclude=${serverpath}/seahub/avatar/testdata --exclude=${serverpath}/seafile/lib/*.la --exclude=${serverpath}/seafile/lib/*.a --exclude=${serverpath}/seafile/lib/pkgconfig* --exclude=${serverpath}/seafile/bin/ccnet-tool --exclude=${serverpath}/seafile/bin/ccnet-servtool --exclude=${serverpath}/seafile/bin/ccnet-web.sh --exclude=${serverpath}/seafile/bin/ccnet-tool --exclude=${serverpath}/seafile/bin/searpc-codegen.py
+
+mv ${serverpath} seafile-server 
