deb74d7590ceb8c5396adc43bca3c89fcec71f2e
lins05 <linshuai2012@gmail.com>
2012-12-20 11:00:25 +0800

    update windows installer to 1.3.5

--- web/main.py
+++ web/main.py
@@ -76,7 +76,7 @@ render = render_mako(directories=['templates'],
 
 app = web.application(urls, globals())
 
-SEAFILE_VERSION = '1.3.0'
+SEAFILE_VERSION = '1.3.5'
 default_options = { "confdir": CCNET_CONF_PATH,
                     'web_ctx': web.ctx, 
                     'seafile_version': SEAFILE_VERSION,
