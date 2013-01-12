28fe4f70a2f04b1abb9afb02c091566e17abb81c
poetwang <poetwang@gmail.com>
2013-01-12 11:56:31 +0800

    [Mac] Only print the base filename instead of full path into the log file

--- gui/common/applet-log.h
+++ gui/common/applet-log.h
@@ -7,13 +7,18 @@
 #include <string.h>
 #include <stdlib.h>
 
+#ifdef __APPLE__
+#define __BASEFILE__ ((strrchr(__FILE__, '/') ?: __FILE__ - 1) + 1)
+#else
+#define __BASEFILE__ __FILE__
+#endif
 
 #define applet_warning(fmt, ...) \
-    g_warning("%s(%d): " fmt, __FILE__, __LINE__, ##__VA_ARGS__)
+    g_warning("%s(%d): " fmt, __BASEFILE__, __LINE__, ##__VA_ARGS__)
 #define applet_message(fmt, ...) \
-    g_message("%s(%d): " fmt, __FILE__, __LINE__, ##__VA_ARGS__)
+    g_message("%s(%d): " fmt, __BASEFILE__, __LINE__, ##__VA_ARGS__)
 #define applet_debug(fmt, ...) \
-    g_debug("%s(%d): " fmt, __FILE__, __LINE__, ##__VA_ARGS__)
+    g_debug("%s(%d): " fmt, __BASEFILE__, __LINE__, ##__VA_ARGS__)
 
 
 int applet_log_init (const char *config_dir);
