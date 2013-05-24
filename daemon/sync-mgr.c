a0eea10bcd6f59305739e35141032d1ed773f177
lins05 <linshuai2012@gmail.com>
2013-05-24 17:37:36 +0800

    [client] make default notify_sync value "on"

--- daemon/sync-mgr.c
+++ daemon/sync-mgr.c
@@ -388,6 +388,11 @@ static gboolean
 need_notify_sync (SeafRepo *repo)
 {
     char *notify_setting = seafile_session_config_get_string(seaf, "notify_sync");
+    if (notify_setting == NULL) {
+        seafile_session_config_set_string(seaf, "notify_sync", "on");
+        return TRUE;
+    }
+
     gboolean result = (g_strcmp0(notify_setting, "on") == 0);
     g_free (notify_setting);
     return result;
