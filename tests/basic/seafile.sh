e19b594ab4d5b1f6a62901b0b6e0961db069d4d1
Jiaqiang Xu <xjqkilling@gmail.com>
2013-08-07 13:08:49 +0800

    Correct testing script seafile.sh.

--- tests/basic/seafile.sh
+++ tests/basic/seafile.sh
@@ -22,7 +22,7 @@ fi
 while [ $# -ge 1 ]; do
   case $1 in
     "1" ) 
-      gnome-terminal -e "${ccnet} -c ${conf1} -D ${debug} -f - --no-multicast"
+      gnome-terminal -e "${ccnet} -c ${conf1} -D ${debug} -f -"
       sleep 3
       gnome-terminal -e "${seaf_daemon} -c ${conf1} -d ${conf1}/seafile-data -w worktree/wt1 -D all -l -"
       ;;
@@ -35,12 +35,12 @@ while [ $# -ge 1 ]; do
       gnome-terminal -e "${seaf_server} -c ${conf2}  -d ${conf2}/seafile-data -D all -f -l - -C"
       ;;
     "3" )
-      gnome-terminal -e "${ccnet} -c ${conf3} -D ${debug} -f - --no-multicast"
+      gnome-terminal -e "${ccnet} -c ${conf3} -D ${debug} -f -"
       sleep 3
       gnome-terminal -e "${seaf_daemon} -c ${conf3} -d ${conf3}/seafile-data -w worktree/wt3 -D all -l -"
       ;;
     "4" )
-      gnome-terminal -e "${ccnet} -c ${conf4} -D ${debug} -f - --no-multicast"
+      gnome-terminal -e "${ccnet} -c ${conf4} -D ${debug} -f -"
       sleep 3
       gnome-terminal -e "${seaf_daemon} -c ${conf4} -d ${conf4}/seafile-data -w worktree/wt4 -D all -l -"
       ;;
