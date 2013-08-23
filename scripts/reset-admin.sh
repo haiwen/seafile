92541a0931f76abc8d39848fc2be33924cf34cb3
lins05 <linshuai2012@gmail.com>
2013-08-23 18:05:44 +0800

    [scripts] make reset-admin.sh support mysql

--- scripts/reset-admin.sh
+++ scripts/reset-admin.sh
@@ -4,14 +4,12 @@ SCRIPT=$(readlink -f "$0")
 INSTALLPATH=$(dirname "${SCRIPT}")
 TOPDIR=$(dirname "${INSTALLPATH}")
 default_ccnet_conf_dir=${TOPDIR}/ccnet
-default_seafile_data_dir=${TOPDIR}/seafile-data
-default_seahub_db=${TOPDIR}/seahub.db
 
 function check_python_executable() {
     if [[ "$PYTHON" != "" && -x $PYTHON ]]; then
         return 0
     fi
-        
+
     if which python2.7 2>/dev/null 1>&2; then
         PYTHON=python2.7
     elif which python27 2>/dev/null 1>&2; then
@@ -21,16 +19,37 @@ function check_python_executable() {
     elif which python26 2>/dev/null 1>&2; then
         PYTHON=python26
     else
-        echo 
+        echo
         echo "Can't find a python executable of version 2.6 or above in PATH"
         echo "Install python 2.6+ before continue."
         echo "Or if you installed it in a non-standard PATH, set the PYTHON enviroment varirable to it"
-        echo 
+        echo
         exit 1
     fi
 }
 
-check_python_executable;
+function read_seafile_data_dir () {
+    seafile_ini=${default_ccnet_conf_dir}/seafile.ini
+    if [[ ! -f ${seafile_ini} ]]; then
+        echo "${seafile_ini} not found. Now quit"
+        exit 1
+    fi
+    seafile_data_dir=$(cat "${seafile_ini}")
+    if [[ ! -d ${seafile_data_dir} ]]; then
+        echo "Your seafile server data directory \"${seafile_data_dir}\" is invalid or doesn't exits."
+        echo "Please check it first, or create this directory yourself."
+        echo ""
+        exit 1;
+    fi
+}
 
-seahub_admin_py=${INSTALLPATH}/seahub/tools/seahub-admin.py
-exec $PYTHON $seahub_admin_py $default_ccnet_conf_dir
+check_python_executable;
+read_seafile_data_dir;
+
+export CCNET_CONF_DIR=${default_ccnet_conf_dir}
+export SEAFILE_CONF_DIR=${seafile_data_dir}
+export PYTHONPATH=${INSTALLPATH}/seafile/lib/python2.6/site-packages:${INSTALLPATH}/seafile/lib64/python2.6/site-packages:${INSTALLPATH}/seahub/thirdpart:$PYTHONPATH
+export PYTHONPATH=${INSTALLPATH}/seafile/lib/python2.7/site-packages:${INSTALLPATH}/seafile/lib64/python2.7/site-packages:$PYTHONPATH
+
+manage_py=${INSTALLPATH}/seahub/manage.py
+exec "$PYTHON" "$manage_py" createsuperuser
