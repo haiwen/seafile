98c516358898610bfb03c02603a914319d10b5a4
lins05 <linshuai2012@gmail.com>
2013-11-14 10:53:05 +0800

    remove gui/ and web/

--- gui/common/translate-commit-desc.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef COMMIT_MSG_H
-#define COMMIT_MSG_H
-
-char *
-translate_commit_desc (const char *input);
-
-#endif
--- web/clean-web.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-CCNET_DIR=`pwd`/..
-
-pushd $CCNET_DIR/seamsg/tests/basic
-./clean.sh
-git checkout .
-popd
-
