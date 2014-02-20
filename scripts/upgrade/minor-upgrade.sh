799069290179f4426631ef42b3aea5090316b2d4
lins05 <linshuai2012@gmail.com>
2014-02-20 17:47:07 +0800

    [script] migrate customization in server update script

--- scripts/upgrade/minor-upgrade.sh
+++ scripts/upgrade/minor-upgrade.sh
@@ -13,46 +13,102 @@ echo "-------------------------------------------------------------"
 echo
 read dummy
 
-echo
-echo "------------------------------"
-echo "migrating avatars ..."
-echo
 media_dir=${INSTALLPATH}/seahub/media
 orig_avatar_dir=${INSTALLPATH}/seahub/media/avatars
 dest_avatar_dir=${TOPDIR}/seahub-data/avatars
-
-# move "media/avatars" directory outside
-if [[ ! -d ${dest_avatar_dir} ]]; then
-    echo
-    echo "Error: avatars directory \"${dest_avatar_dir}\" does not exist" 2>&1
-    echo
-    exit 1
-
-elif [[ ! -L ${orig_avatar_dir}} ]]; then
-    mv ${orig_avatar_dir}/* "${dest_avatar_dir}" 2>/dev/null 1>&2
-    rm -rf "${orig_avatar_dir}"
-    ln -s ../../../seahub-data/avatars ${media_dir}
-fi
-
-# update the symlink seafile-server to the new server version
 seafile_server_symlink=${TOPDIR}/seafile-server-latest
-if [[ -L ${seafile_server_symlink} ]]; then
+seahub_data_dir=${TOPDIR}/seahub-data
+
+function migrate_avatars() {
+    echo
+    echo "------------------------------"
+    echo "migrating avatars ..."
+    echo
+    # move "media/avatars" directory outside
+    if [[ ! -d ${dest_avatar_dir} ]]; then
+        echo
+        echo "Error: avatars directory \"${dest_avatar_dir}\" does not exist" 2>&1
+        echo
+        exit 1
+
+    elif [[ ! -L ${orig_avatar_dir}} ]]; then
+        mv "${orig_avatar_dir}"/* "${dest_avatar_dir}" 2>/dev/null 1>&2
+        rm -rf "${orig_avatar_dir}"
+        ln -s ../../../seahub-data/avatars "${media_dir}"
+    fi
+    echo
+    echo "DONE"
+    echo "------------------------------"
+    echo
+}
+
+function make_media_custom_symlink() {
+    media_symlink=${INSTALLPATH}/seahub/media/custom
+    if [[ -L "${media_symlink}" ]]; then
+        return
+
+    elif [[ ! -e "${media_symlink}" ]]; then
+        ln -s ../../../seahub-data/custom "${media_symlink}"
+        return
+
+
+    elif [[ -d "${media_symlink}" ]]; then
+        cp -rf "${media_symlink}" "${seahub_data_dir}/"
+        rm -rf "${media_symlink}"
+        ln -s ../../../seahub-data/custom "${media_symlink}"
+    fi
+
+}
+
+function move_old_customdir_outside() {
+    # find the path of the latest seafile server folder
+    if [[ -L ${seafile_server_symlink} ]]; then
+        latest_server=$(readlink -f "${seafile_server_symlink}")
+    else
+        return
+    fi
+
+    old_customdir=${latest_server}/seahub/media/custom
+
+    # old customdir is already a symlink, do nothing
+    if [[ -L "${old_customdir}" ]]; then
+        return
+    fi
+
+    # old customdir does not exist, do nothing
+    if [[ ! -e "${old_customdir}" ]]; then
+        return
+    fi
+
+    # media/custom exist and is not a symlink
+    cp -rf "${old_customdir}" "${seahub_data_dir}/"
+}
+
+function update_latest_symlink() {
+    # update the symlink seafile-server to the new server version
     echo
     echo "updating seafile-server-latest symbolic link to ${INSTALLPATH} ..."
     echo
-    if ! rm -f ${seafile_server_symlink}; then
+    if ! rm -f "${seafile_server_symlink}"; then
         echo "Failed to remove ${seafile_server_symlink}"
         echo
         exit 1;
     fi
 
-    if ! ln -s $(basename ${INSTALLPATH}) ${seafile_server_symlink}; then
+    if ! ln -s "$(basename ${INSTALLPATH})" "${seafile_server_symlink}"; then
         echo "Failed to update ${seafile_server_symlink} symbolic link."
         echo
         exit 1;
     fi
+}
 
-fi
+migrate_avatars;
+
+move_old_customdir_outside;
+make_media_custom_symlink;
+
+update_latest_symlink;
+
 
 echo "DONE"
 echo "------------------------------"
