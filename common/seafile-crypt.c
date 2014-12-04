35611d1969219f7915a0ebaf47a75908c890da28
Jiaqiang Xu <xjqkilling@gmail.com>
2014-12-04 10:49:06 +0800

    Modify comments.

--- common/seafile-crypt.c
+++ common/seafile-crypt.c
@@ -19,7 +19,7 @@
 
 #define KEYGEN_ITERATION 1 << 19
 #define KEYGEN_ITERATION2 1000
-/* truly random sequece read from /dev/urandom. */
+/* Should generate random salt for each repo. */
 static unsigned char salt[8] = { 0xda, 0x90, 0x45, 0xc3, 0x06, 0xc7, 0xcc, 0x26 };
 
 SeafileCrypt *
