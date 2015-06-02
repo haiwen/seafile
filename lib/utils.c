0235ef8fedcb11d294aa0ba66debe4d6d01007a2
julienCXX <git@chmodplusx.eu>
2015-06-02 21:02:57 +0200

    fix sendn/recvn prototype

--- lib/utils.c
+++ lib/utils.c
@@ -771,7 +771,7 @@ writen(int fd, const void *vptr, size_t n)
 
 
 ssize_t						/* Read "n" bytes from a descriptor. */
-recvn(int fd, void *vptr, size_t n)
+recvn(evutil_socket_t fd, void *vptr, size_t n)
 {
 	size_t	nleft;
 	ssize_t	nread;
@@ -800,7 +800,7 @@ recvn(int fd, void *vptr, size_t n)
 }
 
 ssize_t						/* Write "n" bytes to a descriptor. */
-sendn(int fd, const void *vptr, size_t n)
+sendn(evutil_socket_t fd, const void *vptr, size_t n)
 {
 	size_t		nleft;
 	ssize_t		nwritten;
