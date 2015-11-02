fe61a096beb0166cd6ac63cd5b4f79c75ac643fa
Shuai Lin <linshuai2012@gmail.com>
2015-11-02 09:43:49 +0800

    fixed reset-admin.sh script
    forgot to export SEAFILE_CENTRAL_CONF_DIR

--- scripts/reset-admin.sh
+++ scripts/reset-admin.sh
@@ -4,6 +4,7 @@ SCRIPT=$(readlink -f "$0")
 INSTALLPATH=$(dirname "${SCRIPT}")
 TOPDIR=$(dirname "${INSTALLPATH}")
 default_ccnet_conf_dir=${TOPDIR}/ccnet
+central_config_dir=${TOPDIR}/conf
 
 function check_python_executable() {
     if [[ "$PYTHON" != "" && -x $PYTHON ]]; then
@@ -48,6 +49,7 @@ read_seafile_data_dir;
 
 export CCNET_CONF_DIR=${default_ccnet_conf_dir}
 export SEAFILE_CONF_DIR=${seafile_data_dir}
+export SEAFILE_CENTRAL_CONF_DIR=${central_config_dir}
 export PYTHONPATH=${INSTALLPATH}/seafile/lib/python2.6/site-packages:${INSTALLPATH}/seafile/lib64/python2.6/site-packages:${INSTALLPATH}/seahub/thirdpart:$PYTHONPATH
 export PYTHONPATH=${INSTALLPATH}/seafile/lib/python2.7/site-packages:${INSTALLPATH}/seafile/lib64/python2.7/site-packages:$PYTHONPATH
 
