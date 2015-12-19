99ca5b5a4bd47e582fed0804291e6e247b4e5aeb
Shuai Lin <linshuai2012@gmail.com>
2015-12-19 13:54:45 +0800

    winserver 5.0

--- scripts/upgrade/win32/py/gc.py
+++ scripts/upgrade/win32/py/gc.py
@@ -5,22 +5,18 @@ import sys
 import traceback
 import ccnet
 
-from upgrade_common import install_path, seafile_dir, ccnet_dir, run_argv
+from upgrade_common import install_path, seafile_dir, ccnet_dir, run_argv, ensure_server_not_running, central_config_dir
 
-def ensure_server_not_running():
-    client = ccnet.SyncClient(ccnet_dir)
-    try:
-        client.connect_daemon()
-    except ccnet.NetworkError:
-        pass
-    else:
-        raise Exception('Seafile server is running! You must turn it off before gc!')
 
 def call_seafserv_gc():
     args = [
         os.path.join(install_path, 'seafile', 'bin', 'seafserv-gc.exe'),
-        '-c', ccnet_dir,
-        '-d', seafile_dir,
+        '-c',
+        ccnet_dir,
+        '-d',
+        seafile_dir,
+        '-F',
+        central_config_dir,
     ]
 
     print 'Starting gc...\n'
@@ -38,6 +34,7 @@ def main():
     finally:
         print '\nprint ENTER to exit\n'
         raw_input()
+
 
 if __name__ == '__main__':
     main()
