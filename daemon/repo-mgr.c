5824e798ede58472c3e195b46466f149f4fc79ce
Jonathan Xu <jonathan.xu@seafile.com>
2018-09-11 15:56:06 +0800

    Force delete all files when deleting a folder recurively.

--- daemon/repo-mgr.c
+++ daemon/repo-mgr.c
@@ -4953,7 +4953,6 @@ delete_worktree_dir_recursive_win32 (struct index_state *istate,
                                      const char *path,
                                      const wchar_t *path_w)
 {
-    struct cache_entry *ce;
     WIN32_FIND_DATAW fdata;
     HANDLE handle;
     wchar_t *pattern;
@@ -5004,17 +5003,17 @@ delete_worktree_dir_recursive_win32 (struct index_state *istate,
             }
         } else {
             /* Files like .DS_Store and Thumbs.db should be deleted any way. */
-            if (!builtin_ignored) {
-                mtime = (guint64)file_time_to_unix_time (&fdata.ftLastWriteTime);
-                ce = index_name_exists (istate, sub_path, strlen(sub_path), 0);
-                if (!ce || (!is_eml_file (dname) && ce->ce_mtime.sec != mtime)) {
-                    seaf_message ("File %s is changed, skip deleting it.\n", sub_path);
-                    g_free (sub_path_w);
-                    g_free (sub_path);
-                    ret = -1;
-                    continue;
-                }
-            }
+            /* if (!builtin_ignored) { */
+            /*     mtime = (guint64)file_time_to_unix_time (&fdata.ftLastWriteTime); */
+            /*     ce = index_name_exists (istate, sub_path, strlen(sub_path), 0); */
+            /*     if (!ce || (!is_eml_file (dname) && ce->ce_mtime.sec != mtime)) { */
+            /*         seaf_message ("File %s is changed, skip deleting it.\n", sub_path); */
+            /*         g_free (sub_path_w); */
+            /*         g_free (sub_path); */
+            /*         ret = -1; */
+            /*         continue; */
+            /*     } */
+            /* } */
 
             if (!DeleteFileW (sub_path_w)) {
                 error = GetLastError();
@@ -5074,7 +5073,6 @@ delete_worktree_dir_recursive (struct index_state *istate,
     GError *error = NULL;
     char *sub_path, *full_sub_path;
     SeafStat st;
-    struct cache_entry *ce;
     int ret = 0;
     gboolean builtin_ignored = FALSE;
 
@@ -5104,16 +5102,16 @@ delete_worktree_dir_recursive (struct index_state *istate,
                 ret = -1;
         } else {
             /* Files like .DS_Store and Thumbs.db should be deleted any way. */
-            if (!builtin_ignored) {
-                ce = index_name_exists (istate, sub_path, strlen(sub_path), 0);
-                if (!ce || ce->ce_mtime.sec != st.st_mtime) {
-                    seaf_message ("File %s is changed, skip deleting it.\n", full_sub_path);
-                    g_free (sub_path);
-                    g_free (full_sub_path);
-                    ret = -1;
-                    continue;
-                }
-            }
+            /* if (!builtin_ignored) { */
+            /*     ce = index_name_exists (istate, sub_path, strlen(sub_path), 0); */
+            /*     if (!ce || ce->ce_mtime.sec != st.st_mtime) { */
+            /*         seaf_message ("File %s is changed, skip deleting it.\n", full_sub_path); */
+            /*         g_free (sub_path); */
+            /*         g_free (full_sub_path); */
+            /*         ret = -1; */
+            /*         continue; */
+            /*     } */
+            /* } */
 
             /* Delete all other file types. */
             if (seaf_util_unlink (full_sub_path) < 0) {
