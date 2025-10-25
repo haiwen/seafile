c39c80f10a6e96e285a678d74c04642dc79f1563
feiniks <36756310+feiniks@users.noreply.github.com>
2025-10-25 16:33:48 +0800

    Cancel context when client is closed (#2963)
    * Cancel context when client is closed
    
    * Add comment
    
    ---------
    
    Co-authored-by: yangheran <heran.yang@seafile.com>

--- daemon/notif-mgr.c
+++ daemon/notif-mgr.c
@@ -429,6 +429,10 @@ event_callback (struct lws *wsi, enum lws_callback_reasons reason,
     case LWS_CALLBACK_CLIENT_CLOSED:
         ret = -1;
         server->status = STATUS_ERROR;
+        // When the client is closed, cancel the context to prevent the socket from blocking in poll after the operating system wakes from sleep.
+        // After calling lws_cancel_service, a LWS_CALLBACK_EVENT_WAIT_CANCELLED callback is sent to every protocol on every vhost,
+        // notification_worker will exit loop.
+        lws_cancel_service (server->context);
         break;
     case LWS_CALLBACK_EVENT_WAIT_CANCELLED:
         ret = -1;
